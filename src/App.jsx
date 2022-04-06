64334f5544c2d51f7db7395f588101e3c15fc651
Miguel Ibarra <52647412+mibar0005@users.noreply.github.com>
2022-04-05 19:01:34 -0700

    Netflix User Interface
    Completed most if not all of the pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Register from './pages/register/Register';
+import './App.scss';
+import Login from './pages/login/Login';
+import Watch from './pages/watch/Watch'
+import Home from './pages/home/Home'
+
+
+const App = () => {
+  return (
+  <div>
+    <Home />
+  </div>
+  );
+};
+
+export default App;
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,57 @@
+import "./Login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="container">
+        <form>
+          <h1>Sign In</h1>
+          <input type="email" placeholder="Email or phone number" />
+          <input type="password" placeholder="Password" />
+          <button className="loginButton">Sign In</button>
+          <span>
+            New to Netflix? <b>Sign up now.</b>
+          </span>
+          <small>This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <b>Learn more</b> </small>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
+{
+  /* <div className="container">
+        <h1>Unlimited movies, TV shows, and more.</h1>
+        <h2>Watch anywhere. Cancel anytime.</h2>
+        <p>
+          Ready to watch? Enter your email to create or restart your membership.
+        </p>{" "}
+        {!email ? (
+          <div className="input">
+            <input type="email" placeholder="email address" ref={emailRef} />
+            <button className="registerButton" onClick={handleStart}>
+              Get Started
+            </button>
+          </div>
+        ) : (
+          <form className="input">
+            <input type="password" placeholder="password" ref={passwordRef} />
+            <button className="registerButton" onClick={handleFinish}>
+              Start
+            </button>
+          </form>
+        )}
+      </div> */
+}
